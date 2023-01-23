2b53bdec6d492656d43f7bef31c21c1f035e8428
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2023-01-23 12:53:17 +0000

    punto 4

--- annunci-cognome/src/app/app.component.ts
+++ annunci-cognome/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Annunci } from './models/annunci.models';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   title = 'annunci-cognome';
+  annunci_vet : Annunci[] = []
+  invia(nome :HTMLInputElement,tele :HTMLInputElement,mess :HTMLInputElement ){
+    this.annunci_vet.push(new Annunci(nome.value,tele.value,mess.value))
+    console.log(this.annunci_vet)
+  }
 }
